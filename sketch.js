59efdf6f63b45859b8d8a607cb7f34d2a4e070c7
Peter Cowal <pcowal@gmail.com>
2022-05-16 23:42:07 -0700

    adds ability to highlight single data point

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let formantTable;
 let displayFont;
 
-let checkbox3D, checkboxFullscreen;
+let checkbox3D, checkboxFullscreen, checkboxViewRecent;
 
 let c_green, c_green_transparent, c_aqua, c_aqua_transparent, c_blue, c_blue_transparent;
 
@@ -9,7 +9,6 @@ let graph_x = -500;
 let graph_y = -250;
 let graph_w = 1000;
 let graph_h = 500;
-
 
 let graph_rotateX = 0;
 let graph_rotateZ = 0;
@@ -25,6 +24,9 @@ let max_f2 = 4000;
 const VOWEL_I = 1;
 const VOWEL_U = 2;
 const VOWEL_A = 3;
+
+// view recent animation code
+let viewRecentTimer = 0;
 
 const dataURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQWknPGSkd4sGgjSSs1ajnRZnmPcX58spNmN1v8zeM5VQxkm4AjwIPSU-9KNAVicU_HaKU0T0bIBbwu/pub?output=csv';
 const googleSheetURL = 'https://sheets.googleapis.com/v4/spreadsheets/1d9lbDIsLw6rhTJwtOdmujzTiBPexV9Nr_0qH4hdmLsw/values/Form%20Responses%201?key=AIzaSyBY-hngRiU7OoIeruB0WuIXycsE2ufbHa0';
@@ -43,6 +45,13 @@ function loadGoogleSheetData(data) {
     }
   }
 }
+
+function smoothstep(x) {
+  if (x<0) return 0;
+  if (x>1) return 1;
+  return 3*x*x - 2*x*x*x;
+}
+
 
 function preload() {
   //formantTable = loadTable('data/formants.csv', 'csv', 'header');
@@ -67,7 +76,6 @@ function setup() {
   graph_x = -graph_w/2;
   graph_y = -graph_h/2;
 
-
   textFont(displayFont);
 
   c_green = color(50, 255, 10);
@@ -79,11 +87,15 @@ function setup() {
 
 
   checkbox3D = createCheckbox('3D View', false);
-  checkbox3D.position(0,0);
+  checkbox3D.position(5,25);
 
   checkboxFullscreen = createCheckbox('Fullscreen', false);
-  checkboxFullscreen.position(0, 16);
+  checkboxFullscreen.position(5, 5);
   checkboxFullscreen.changed(toggleFullscreen);
+
+  checkboxViewRecent = createCheckbox('View Recent', false);
+  checkboxViewRecent.position(5,45);
+  checkboxViewRecent.changed(highlightRecent);
 
   print(formantTable.getRowCount() + ' total rows in table');
   print(formantTable.getColumnCount() + ' total columns in table');
@@ -153,7 +165,7 @@ function textBillboard(str, x, y, z) {
   pop();
 }
 
-function drawDataPoint(f1, f2, f0, vowel, size) {
+function drawDataPoint(f1, f2, f0, vowel, size, solid) {
 
   let x = f2ToX(f2);
   let y = f1ToY(f1);
@@ -172,12 +184,21 @@ function drawDataPoint(f1, f2, f0, vowel, size) {
 
   if (vowel == VOWEL_A) {
     stroke(c_aqua);
+    if (solid) {
+      fill(c_aqua);
+    }
     square(-size/2, -size/2, size);
   } else if (vowel == VOWEL_I) {
     stroke(c_green);
+    if (solid) {
+      fill(c_green);
+    }
     triangle(-size/2, -size*0.29, size/2, -size*0.29, 0, size*0.58);
   } else if (vowel == VOWEL_U) {
     stroke(c_blue);
+    if (solid) {
+      fill(c_blue);
+    }
     circle(0, 0, size);
   }
   pop();
@@ -185,12 +206,9 @@ function drawDataPoint(f1, f2, f0, vowel, size) {
 
 function draw() {
   background(40);
-
-
   let rotateX_target = 0;
 
   if(checkbox3D.checked()) {
-
     rotateX_target = PI/3;
     graph_rotateZ += 0.0002*deltaTime;
     if (graph_rotateZ > PI) {
@@ -233,10 +251,43 @@ function draw() {
     textBillboard(f2, x, graph_y - 5, 0);
   }
 
-  for (let i = 0; i < formantTable.getRowCount(); i++) {
-    drawDataPoint(formantTable.getNum(i, 1), formantTable.getNum(i, 2), formantTable.getNum(i, 3), VOWEL_I, 8);
-    drawDataPoint(formantTable.getNum(i, 4), formantTable.getNum(i, 5), formantTable.getNum(i, 6), VOWEL_U, 8);
-    drawDataPoint(formantTable.getNum(i, 7), formantTable.getNum(i, 8), formantTable.getNum(i, 9), VOWEL_A, 8);
+
+  let n = formantTable.getRowCount()-1;
+
+  let recentSolid = false;
+  let recentSize = 8;
+
+  if (checkboxViewRecent.checked()) {
+    recentSize = 15 - 7 * cos(viewRecentTimer*30) * exp(-2*viewRecentTimer);
+  } else {
+    recentSize = 8 + 7 * smoothstep(20 - viewRecentTimer);
+  }
+
+  recentSolid = true;
+
+  drawDataPoint(formantTable.getNum(n, 1), formantTable.getNum(n, 2), formantTable.getNum(n, 3), VOWEL_I, recentSize, recentSolid);
+  drawDataPoint(formantTable.getNum(n, 4), formantTable.getNum(n, 5), formantTable.getNum(n, 6), VOWEL_U, recentSize, recentSolid);
+  drawDataPoint(formantTable.getNum(n, 7), formantTable.getNum(n, 8), formantTable.getNum(n, 9), VOWEL_A, recentSize, recentSolid);
+
+
+  if (!checkboxViewRecent.checked()) {
+    for (let i = 0; i < formantTable.getRowCount()-1; i++) {
+      let tween = pow(smoothstep(1 + 0.1*i - viewRecentTimer), 5);
+      let f1 = formantTable.getNum(i, 1) * (1-tween) + formantTable.getNum(n, 1) * tween;
+      let f2 = formantTable.getNum(i, 2) * (1-tween) + formantTable.getNum(n, 2) * tween;
+      let f0 = formantTable.getNum(i, 3) * (1-tween) + formantTable.getNum(n, 3) * tween;
+      drawDataPoint(f1, f2, f0, VOWEL_I, 8, false);
+
+      f1 = formantTable.getNum(i, 4) * (1-tween) + formantTable.getNum(n, 4) * tween;
+      f2 = formantTable.getNum(i, 5) * (1-tween) + formantTable.getNum(n, 5) * tween;
+      f0 = formantTable.getNum(i, 6) * (1-tween) + formantTable.getNum(n, 6) * tween;
+      drawDataPoint(f1, f2, f0, VOWEL_U, 8, false);
+
+      f1 = formantTable.getNum(i, 7) * (1-tween) + formantTable.getNum(n, 7) * tween;
+      f2 = formantTable.getNum(i, 8) * (1-tween) + formantTable.getNum(n, 8) * tween;
+      f0 = formantTable.getNum(i, 9) * (1-tween) + formantTable.getNum(n, 9) * tween;
+      drawDataPoint(f1, f2, f0, VOWEL_A, 8, false);
+    }
   }
 
   textSize(50);
@@ -249,7 +300,17 @@ function draw() {
   //drawDataPoint(findColumnAverage(formantTable, 1), findColumnAverage(formantTable, 2),, VOWEL_I, 22);
 
 
+  viewRecentTimer += deltaTime/1000;
 }
+
+function highlightRecent() {
+  // whenever this is toggled, update data
+  if (checkboxViewRecent.checked()) {
+      loadJSON(googleSheetURL, loadGoogleSheetData);
+  }
+  viewRecentTimer = 0;
+}
+
 
 function toggleFullscreen() {
   fullscreen(checkboxFullscreen.checked());
